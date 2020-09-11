985d103e9d5d99b0036a1be947739aacbb7bec25
CheckMan1707 <70796365+CheckMan1707@users.noreply.github.com>
2020-09-11 19:17:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,172 +2,94 @@ import time
 import json
 import requests
 import random
-import os
+import datetime
 
 #sectets字段录入
 deptId = eval(input())
 text = input()
-emergencyNum = input()
-phoneNum = input()
 userName = input()
 stuNum = input()
 userId = input()
-dormNum = input()
-homeTown = input()
-personNum = input()
-homeAddress = input()
-local = input()
-sex = "男"
-area = {'address':homeAddress,'text':local,'code':""}
-areaStr = json.dumps(area, ensure_ascii=False)
 
 #随机温度(36.2~36.8)
 a=random.uniform(36.2,36.8)
 temperature = round(a, 1)
 
+#时间获取
+cstTime = (datetime.datetime.utcnow() + datetime.timedelta(hours=8))
+strTime = cstTime.strftime("%H:%M:%S")
+
+#reqURl
 sign_url = "https://reportedh5.17wanxiao.com/sass/api/epmpics"
 
-json =  {
+#早中午判断
+nowTime = time.localtime().tm_hour + 8
+if (nowTime >= 6) & (nowTime < 8):
+    templateid = "clockSign1"
+    RuleId = 146
+elif (nowTime >= 12) & (nowTime < 14):
+    templateid = "clockSign2"
+    RuleId = 147
+elif (nowTime >= 21) & (nowTime< 22):
+    templateid = "clockSign3"
+    RuleId = 148
+else:
+    print("现在时间%d点%d分，打卡时间未到！" %(nowTime,time.localtime().tm_min))
+    exit(0)
+
+jsons =  {
     "businessType": "epmpics",
-    "method": "submitUpInfo",
+    "method": "submitUpInfoSchool",
     "jsonData": {
         "deptStr": {
             "deptid": deptId,
             "text": text
         },
-        "areaStr": areaStr,
+        "areaStr": "{\"streetNumber\":\"\",\"street\":\"长椿路辅路\",\"district\":\"中原区\",\"city\":\"郑州市\",\"province\":\"河南省\",\"town\":\"\",\"pois\":\"河南工业大学(莲花街校区)\",\"lng\":113.55064699999795,\"lat\":34.83870696238093,\"address\":\"中原区长椿路辅路河南工业大学(莲花街校区)\",\"text\":\"河南省-郑州市\",\"code\":\"\"}",
         "reportdate": round(time.time()*1000),
         "customerid": "43",
         "deptid": deptId,
         "source": "app",
-        "templateid": "pneumonia",
+        "templateid": templateid,
         "stuNo": stuNum,
         "username": userName,
-        "phonenum": phoneNum,
         "userid": userId,
-        "updatainfo": [
-            {
-                "propertyname": "isGoWarningAdress",
-                "value": sex
-            },
-            {
-                "propertyname": "jtdz",
-                "value": homeTown
-            },
-            {
-                "propertyname": "personNO",
-                "value": personNum
-            },
-            {
-                "propertyname": "langtineadress",
-                "value": homeAddress
-            },
-            {
-                "propertyname": "ownPhone",
-                "value": phoneNum
-            },
-            {
-                "propertyname": "emergencyContact",
-                "value": emergencyNum
-            },
-            {
-                "propertyname": "tradeNum",
-                "value": dormNum
-            },
+        "updatainfo": [  
             {
                 "propertyname": "temperature",
                 "value": temperature
             },
             {
                 "propertyname": "symptom",
-                "value": "均无"
-            },
-            {
-                "propertyname": "isContactpatient",
-                "value": "均无"
-            },
-            {
-                "propertyname": "istouchcb",
-                "value": "否"
-            },
-            {
-                "propertyname": "isTransitProvince",
-                "value": "否"
-            },
-            {
-                "propertyname": "isTouch",
-                "value": "否"
-            },
-            {
-                "propertyname": "backadress",
-                "value": ""
-            },
-            {
-                "propertyname": "isContactFriendIn14",
-                "value": "否"
-            },
-            {
-                "propertyname": "sxaddress",
-                "value": ""
-            },
-            {
-                "propertyname": "medicalObservation",
-                "value": "否"
-            },
-            {
-                "propertyname": "sxss",
-                "value": ""
-            },
-            {
-                "propertyname": "isConfirmed",
-                "value": "否"
-            },
-            {
-                "propertyname": "assistRemark",
-                "value": ""
-            },
-            {
-                "propertyname": "gyfh",
-                "value": "否"
-            },
-            {
-                "propertyname": "FamilyIsolate",
-                "value": ""
-            },
-            {
-                "propertyname": "ishborwh",
-                "value": "否"
-            },
-            {
-                "propertyname": "IsHospitaltxt",
-                "value": ""
-            },
-            {
-                "propertyname": "fhhb",
-                "value": "否"
-            },
-            {
-                "propertyname": "isname",
-                "value": ""
-            },
-            {
-                "propertyname": "other1",
-                "value": ""
-            },
-            {
-                "propertyname": "isFFHasSymptom",
-                "value": "是"
+                "value": "无症状"
             }
         ],
-        "gpsType": 1
-    }
-}                       
-response = requests.post(sign_url, json=json)
+        "customerAppTypeRuleId": RuleId,
+        "clockState": 0
+    },
+}    
+
+#提交打卡
+response = requests.post(sign_url, json=jsons)
 print(response.text)
 
+#结果判定
+if response.json()["msg"] == '成功':
+        msg = "打卡成功-" + strTime
+else:
+        msg = "打卡异常-" + strTime
+print(msg)
+
+#微信通知
 sckey = input()
-title = "打卡结果通知"
-content = response.text
+title = msg
+result = json.dumps(response.json(), sort_keys=True, indent=4, separators=(',', ': '),ensure_ascii=False)
+content = f"""
+```
+{result}
+```
+### 😀[收藏](https://github.com/YooKing/HAUT_autoCheck)此项目
+"""
 data = {
 "text":title,
 "desp":content
